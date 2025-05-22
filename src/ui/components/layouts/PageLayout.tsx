62d2449cbf1554fded2b9fcc2bb06c5e4f07625a
Cornelius Okeke <okekecornelius@gmail.com>
2025-05-22 17:19:40 +0100

    setup a working electron-react successfully

---
+++ src/ui/components/layouts/PageLayout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+	className?: string;
+	children: ReactNode;
+}
+
+export default function PageLayoutComponent({ className, children }: Props) {
+	return <div className={twMerge('p-3', className)}>{children}</div>
+}
